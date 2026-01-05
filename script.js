54d7799531ca5d7b9e69b32d660f7a54b905c73c
evygoddess-21 <evymiranda18@gmail.com>
2026-01-04 19:20:15 -0500

    script.js

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 let rotation = 0;
 
-// CONEXIÓN WEBSOCKET
-const socket = new WebSocket("WSS_DEL_LISTENER");
-
-socket.onmessage = (event) => {
-  const data = JSON.parse(event.data);
-
-  if (data.type === "tip") {
-    spin();
-  }
-};
-
 function spin() {
   const extra = Math.floor(Math.random() * 360);
   rotation += 1440 + extra;
   document.getElementById("wheel").style.transform =
     `rotate(${rotation}deg)`;
 }
+
+// PRUEBA: girar con la tecla G
+document.addEventListener("keydown", (e) => {
+  if (e.key === "g") {
+    spin();
+  }
+});
